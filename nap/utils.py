02d2d387e99ee0a9296a9424ed47c878dc9cb28a
Curtis Maloney <curtis@tinbrain.net>
2012-12-28 17:12:00 +1100

    Added utils.digattr

---
+++ nap/utils.py
@@ -0,0 +1,19 @@
+
+def digattr(obj, attr, default=None):
+    '''Perform template-style dotted lookup'''
+    steps = attr.split('.')
+    for step in steps:
+        try:    # dict lookup
+            obj = obj[step]
+        except (TypeError, AttributeError, KeyError):
+            try:    # attribute lookup
+                obj = getattr(obj, step)
+            except (TypeError, AttributeError):
+                try:    # list index lookup
+                    obj = obj[int(step)]
+                except (IndexError, ValueError, KeyError, TypeError):
+                    return default
+        if callable(obj):
+            obj = obj()
+    return obj
+
