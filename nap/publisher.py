bf6e60005df39904a0ab177e5772d3143014ffd8
Curtis Maloney <curtis@tinbrain.net>
2012-12-28 18:17:34 +1100

    Added first-pass at Pagination

--- nap/publisher.py
+++ nap/publisher.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import url
+from django.core.paginator import Paginator
 from django.views.generic.base import View
 from django.http import HttpResponse
 
@@ -30,6 +31,7 @@ class Publisher(View):
         /(id)/(action)/     custom action on instance
         '''
         self.action = action
+        self.request = request  # Shouldn't the as_view wrapper do this?
         method = request.method.lower()
         if object_id is None:
             # Look for list handler
@@ -50,10 +52,32 @@ class Publisher(View):
     def get_object(self, object_id):
         raise NotImplementedError
 
+    def get_page(self, object_list):
+        page_size = getattr(self, 'page_size', None)
+        if page_size is None:
+            return {
+                'meta': {},
+                'objects': object_list,
+            }
+        paginator = Paginator(object_list, page_size)
+        offset = int(self.request.GET.get('offset', 0))
+        page_num = offset // page_size
+        page = paginator.page(page_num or 1)
+        return {
+            'meta': {
+                'offset': page.start_index(),
+                'limit': page_size,
+                'count': paginator.count,
+            },
+            'objects': page.object_list,
+        }
+
     def get_default_list(self, request, **kwargs):
         object_list = self.get_object_list()
         serialiser = self.get_serialiser(request, **kwargs)
-        return self.render_to_response(serialiser.deflate_list(object_list))
+        data = self.get_page(object_list)
+        data['objects'] = serialiser.deflate_list(data['objects'])
+        return self.render_to_response(data)
 
     def get_default_object(self, request, object_id, **kwargs):
         # XXX Make 'pk' configurable?
