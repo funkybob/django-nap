f95fc0f0f75733c1a743d5bf430cc4fcd48dcf8c
Curtis Maloney <curtis@tinbrain.net>
2012-12-28 18:39:56 +1100

    Added undigattr

--- nap/utils.py
+++ nap/utils.py
@@ -17,3 +17,22 @@ def digattr(obj, attr, default=None):
             obj = obj()
     return obj
 
+def undigattr(obj, attr, value):
+    steps = attr.split('.')
+    steps.reverse()
+    last = steps.pop(0)
+    for step in steps:
+        try:
+            obj = obj[step]
+        except (TypeError, AttributeError, KeyError):
+            try:    # attribute lookup
+                obj = getattr(obj, step)
+            except (TypeError, AttributeError):
+                try:    # list index lookup
+                    obj = obj[int(step)]
+                except (IndexError, ValueError, KeyError, TypeError):
+                    return default
+        if callable(obj):
+            obj = obj()
+    setattr(obj, last, value)
+
