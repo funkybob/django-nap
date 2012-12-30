c60bf351ab99c985a6ec32cdb3b5b74ad6119551
Curtis Maloney <curtis@tinbrain.net>
2012-12-30 13:12:44 +1100

    Moved JSON code into HTTP module

--- nap/http.py
+++ nap/http.py
@@ -7,3 +7,13 @@ from functools import partial
 HttpCreated = partial(HttpResponse, status_code=202)
 HttpNoContent = partial(HttpResponse, status_code=204)
 
+from utils import JSONEncoder
+
+json = JSONEncoder()
+
+class JsonResponse(HttpResponse):
+    '''Handy shortcut for dumping JSON data'''
+    def __init__(self, content, *args, **kwargs):
+        kwargs.setdefault('content_type', 'application/json')
+        super(JsonResponse, self).__init__(json.dumps(content), *args, **kwargs)
+
