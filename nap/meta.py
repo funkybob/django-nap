2ed366ace6fdd348e3ce83aed2c475fdf534cabc
Curtis Maloney <curtis@tinbrain.net>
2013-01-02 09:36:31 +1100

    Added common meta class

---
+++ nap/meta.py
@@ -0,0 +1,13 @@
+
+class Meta(object):
+    '''Generic container for Meta classes'''
+
+    def __new__(cls, meta=None):
+        # Return a new class base on ourselves
+        attrs = dict(
+            (name, getattr(meta, name))
+            for name in dir(meta)
+            if not name[0] == '_'
+        )
+        return object.__new__(type('Meta', (cls,), attrs))
+
