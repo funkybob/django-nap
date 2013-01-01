51c19b278da47ffda4274b2f25aee8e9e346a66f
Curtis Maloney <curtis@tinbrain.net>
2013-01-02 10:35:23 +1100

    Register Publishers in API by name. Support "api_name" on Publishers

--- nap/api.py
+++ nap/api.py
@@ -6,16 +6,25 @@ class Api(object):
     '''Helper class for registering many Publishers in one URL namespace'''
     def __init__(self, name):
         self.name = name
-        self.children = set()
+        self.children = {}
 
     def patterns(self):
         urlpatterns = []
         for child in self.children:
             urlpatterns.extend(child.patterns())
         return [
-            (r'^%s/' % self.name, include(urlpatterns))),
+            (r'^%s/' % self.name, include([
+                (r'^%s/' % name, include(child.patterns()))
+                for name, child in self.children.items()
+            ]))
         ]
 
-    def register(self, child):
-        self.children.add(child)
+    def register(self, child, name=None):
+        if name is None:
+            name = getattr(child, 'api_name', child.__class__.__name__.lower())
+        if name in self.children:
+            raise Warning('Publisher with name %s already registered: %r -> %r' % (
+                name, self.children[name], child
+            ))
+        self.children[name] = child
 
