4d7ffb1b09c861a28da3acaae94ee84cb9ee85b7
Curtis Maloney <curtis@tinbrain.net>
2013-01-02 11:20:03 +1100

    Add flat patterns for Api Add register/autodiscover system for Api

--- nap/api.py
+++ nap/api.py
@@ -1,4 +1,5 @@
 
+from django.conf.urls import url, include
 
 # TODO: Add other patterns to allow introspection?
 
@@ -8,15 +9,15 @@ class Api(object):
         self.name = name
         self.children = {}
 
-    def patterns(self):
-        urlpatterns = []
-        for child in self.children:
-            urlpatterns.extend(child.patterns())
+    def patterns(self, flat=False):
+        urlpatterns = [
+            url(r'^%s/' % name, include(child.patterns()))
+            for name, child in self.children.items()
+        ]
+        if flat:
+            return urlpatterns
         return [
-            (r'^%s/' % self.name, include([
-                (r'^%s/' % name, include(child.patterns()))
-                for name, child in self.children.items()
-            ]))
+            url(r'^%s/' % self.name, include(urlpatterns)),
         ]
 
     def register(self, child, name=None):
@@ -27,4 +28,39 @@ class Api(object):
                 name, self.children[name], child
             ))
         self.children[name] = child
+
+APIS = {}
+
+def register(name, *args):
+    try:
+        api = APIS[name]
+    except KeyError:
+        api = APIS[name] = Api(name=name)
+    for resource in args:
+        api.register(resource)
+    return api
+
+def autodiscover():
+    from django.conf import settings
+    from django.utils.importlib import import_module
+    from django.utils.module_loading import module_has_submodule
+
+    for app in settings.INSTALLED_APPS:
+        mod = import_module(app)
+        # Attempt to import the app's api module.
+        try:
+            import_module('%s.serialiser' % app)
+        except:
+
+            # Decide whether to bubble up this error. If the app just
+            # doesn't have an admin module, we can ignore the error
+            # attempting to import it, otherwise we want it to bubble up.
+            if module_has_submodule(mod, 'serialiser'):
+                raise
+
+def patterns(flat=False):
+    patterns = []
+    for api in APIS.values():
+        patterns.extend(api.patterns(flat=flat))
+    return patterns
 
