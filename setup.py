08b4cc4e065e63eef522756888fa8a75d9bf6ddb
Curtis <curtis@tinbrain.net>
2013-01-25 13:11:06 +1100

    Fix find packages Bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-nap',
-    version='0.3',
+    version='0.4',
     description='A light REST tool for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     url='http://github.com/funkybob/django-nap',
     keywords=['django', 'json', 'rest'],
-    packages = find_packages(),
+    packages = find_packages(excludes=['test.*']),
     zip_safe=False,
     classifiers = [
         'Environment :: Web Environment',
