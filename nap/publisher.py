7fe97f63673109334063194c4a9a791c2c0b01c3
Curtis Maloney <curtis@tinbrain.net>
2013-01-30 10:32:32 +1100

    Allow optional url argument to Object urls, also

--- nap/publisher.py
+++ nap/publisher.py
@@ -29,11 +29,12 @@ class Publisher(object):
             return self.dispatch(request, *args, **kwargs)
 
         return [
-            url(r'^object/(?P<object_id>[-\w]+)/(?P<action>\w+)/?$',    view),
-            url(r'^object/(?P<object_id>[-\w]+)/?$',                    view),
-            url(r'^(?P<action>\w+)/(?P<argument>.+)/$',                 view),
-            url(r'^(?P<action>\w+)/?$',                                 view),
-            url(r'^$',                                                  view),
+            url(r'^object/(?P<object_id>[-\w]+)/(?P<action>\w+)/(?P<argument>.+)/?$', view),
+            url(r'^object/(?P<object_id>[-\w]+)/(?P<action>\w+)/?$', view),
+            url(r'^object/(?P<object_id>[-\w]+)/?$', view),
+            url(r'^(?P<action>\w+)/(?P<argument>.+)/$', view),
+            url(r'^(?P<action>\w+)/?$', view),
+            url(r'^$', view),
         ]
 
     def dispatch(self, request, action='default', object_id=None, **kwargs):
