4b241b89acfa20d4f0ff2a827ff7b2c3817377ab
Curtis Maloney <curtis@tinbrain.net>
2013-02-09 10:35:36 +1100

    Removed msgpack

--- nap/msgpack.py
+++
@@ -1,9 +0,0 @@
-
-from django.http import HttpResponse
-import msgpack
-
-class MsgPackRepsponse(HttpResponse):
-    def __init__(self, content, *args, **kwargs):
-        kwargs.setdefault('content_type', 'application/msgpack')
-        super(MsgPackResponse, self).__init__(content, *args, **kwargs)
-
