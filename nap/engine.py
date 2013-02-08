838a9d7a73534b04658ebcbb370a304e5d310df0
Curtis Maloney <curtis@tinbrain.net>
2013-02-09 10:44:35 +1100

    Added engines

---
+++ nap/engine.py
@@ -0,0 +1,31 @@
+
+import json
+try:
+    import msgpack
+except ImportError:
+    pass
+
+class Engine(object):
+    # The list of content types we match
+    CONTENT_TYPES = []
+    def dumps(self, data):
+        '''How to serialiser an object'''
+        raise NotImplementedError
+    
+    def loads(self, data):
+        '''How to deserialise a string'''
+        raise NotImplementedError
+
+class JsonEngine(Engine):
+    CONTENT_TYPES = ['application/json',]
+    def dumps(self, data):
+        return json.dumps(data)
+    def loads(self, data):
+        return json.loads(data)
+
+class MsgPackEngine(Engine):
+    CONTENT_TYPES = ['application/msgpack',] # XXX ??
+    def dumps(self, data):
+        return msgpack.dumps(data)
+    def loads(self, data):
+        return msgpack.loads(data)
