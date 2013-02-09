8000ec8cfa7a0fbd3ad6a42fab814b2818ae988e
Curtis Maloney <curtis@tinbrain.net>
2013-02-09 23:54:11 +1100

    Need to keep doing this until we have typed fields

--- nap/engine.py
+++ nap/engine.py
@@ -5,6 +5,19 @@ try:
 except ImportError:
     pass
 
+from decimal import Decimal
+from datetime import date, datetime, time
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        if isinstance(obj, (datetime, time,)):
+            return obj.replace(microsecond=0).isoformat()
+        if isinstance(obj, date):
+            return obj.isoformat()
+        return super(JsonEncoder, self).default(obj)
+
 class Engine(object):
     # The list of content types we match
     CONTENT_TYPES = []
@@ -19,12 +32,12 @@ class Engine(object):
 class JsonEngine(Engine):
     CONTENT_TYPES = ['application/json',]
     def dumps(self, data):
-        return json.dumps(data)
+        return json.dumps(data, cls=JsonEncoder)
     def loads(self, data):
         return json.loads(data)
 
 class MsgPackEngine(Engine):
-    CONTENT_TYPES = ['application/msgpack',] # XXX ??
+    CONTENT_TYPES = ['application/x-msgpack',]
     def dumps(self, data):
         return msgpack.dumps(data)
     def loads(self, data):
