6025bcc8f4de8135b7755b906640476ec4b46fd7
Curtis Maloney <curtis@tinbrain.net>
2013-02-11 09:44:23 +1100

    Bumped version to 0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-nap',
-    version='0.6',
+    version='0.7',
     description='A light REST tool for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
