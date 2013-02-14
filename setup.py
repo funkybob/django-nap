2d007634db1c1494f3ff4c323fcd5ed5eafa3189
Curtis Maloney <curtis@tinbrain.net>
2013-02-14 11:38:47 +1100

    Bumped version to 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-nap',
-    version='0.7.1',
+    version='0.8',
     description='A light REST tool for Django',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
