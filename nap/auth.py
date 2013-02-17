325a46b1d0e22c69cc5f41fbc31bac455eea8ec8
Curtis Maloney <curtis@tinbrain.net>
2013-02-18 10:14:04 +1100

    Add BaseAuthorise helper mixin Add permit decorators

--- nap/auth.py
+++ nap/auth.py
@@ -1,5 +1,6 @@
 
 # Authentication and Authorisation
+from functools import wraps
 
 class NullAuthorise(object):
 
@@ -32,4 +33,43 @@ class DjangoAuthorise(object):
                     opts.get_delete_permission()
                 ]))
         return False
+
+
+class BaseAuthorise(object):
+    '''
+    Helper class to make targeting specific endpoints easier.
+
+    Follows the same pattern as finding a handler, but prefixes
+    the method name with 'may_'
+    '''
+    may_default = True
+    def may(self, request, action, object_id=None, **kwargs):
+        prefix = 'list' if object_id is None else 'object'
+        method = request.method.lower()
+        handler = getattr(self, 'may_%s_%s_%s' % (
+            prefix,
+            method,
+            action
+        ), None)
+        if handler is None:
+            handler = getattr(self, 'may_%s_%s' % (
+                prefix, action,
+            ), None)
+        if handler is None:
+            return self.may_default
+        return handler(request, action, object_id, **kwargs)
+        
+def permit(test_func):
+    '''Decorate a handler to control access'''
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(self, *args, **kwargs):
+            if test_func(self, *args, **kwargs):
+                return view_func(*args, **kwargs)
+            return http.HttpResponseUnauthorized()
+        return _wrapped_view
+    return decorator
+
+permit_logged_in = permit(lambda self, request, *args, **kwargs: request.user.is_authenticated())
+permit_staff = permit(lambda self, request, *args, **kwargs: request.user.is_staff)
 
