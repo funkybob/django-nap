7603c91c5feff7b2818ee5158dc6b739e16849bb
Curtis Maloney <curtis@tinbrain.net>
2013-02-20 16:08:29 +1100

    Tidy up utils

--- nap/utils.py
+++ nap/utils.py
@@ -1,8 +1,4 @@
 
-import json
-from decimal import Decimal
-import datetime
-
 
 def digattr(obj, attr, default=None):
     '''Perform template-style dotted lookup'''
@@ -22,22 +18,3 @@ def digattr(obj, attr, default=None):
             obj = obj()
     return obj
 
-
-def undigattr(obj, attr, value, default=None):
-    steps = attr.split('.')
-    steps.reverse()
-    last = steps.pop(0)
-    for step in steps:
-        try:
-            obj = obj[step]
-        except (TypeError, AttributeError, KeyError):
-            try:    # attribute lookup
-                obj = getattr(obj, step)
-            except (TypeError, AttributeError):
-                try:    # list index lookup
-                    obj = obj[int(step)]
-                except (IndexError, ValueError, KeyError, TypeError):
-                    return default
-        if callable(obj):
-            obj = obj()
-    setattr(obj, last, value)
