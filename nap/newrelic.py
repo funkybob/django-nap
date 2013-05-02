0d4495755783ee2b2a3757e796b505906152cf17
Curtis <curtis@tinbrain.net>
2013-05-02 15:06:23 +1000

    Added NewRelic support module

---
+++ nap/newrelic.py
@@ -0,0 +1,34 @@
+# Put this on module path in file newrelic_django_nap.py.
+#
+# In agent configuration file add:
+#
+# [import-hook:nap.publisher]
+# enabled = true
+# execute = newrelic_django_nap:instrument_django_nap_publisher
+ 
+from newrelic.agent import (current_transaction, set_transaction_name,
+    FunctionTrace, callable_name, ObjectWrapper)
+ 
+def instrument_django_nap_publisher(module):
+ 
+    def execute_wrapper(wrapped, instance, args, kwargs):
+ 
+        transaction = current_transaction()
+ 
+        if transaction is None:
+            return wrapped(*args, **kwargs)
+ 
+        def _handler(handler, *args, **kwargs):
+            return handler
+ 
+        handler = _handler(*args, **kwargs)
+ 
+        name = callable_name(handler)
+ 
+        set_transaction_name(name)
+ 
+        with FunctionTrace(transaction, name):
+            return wrapped(*args, *kwargs)
+ 
+    module.BasePublisher.execute = ObjectWrapper(
+        module.BasePublisher.execute, None, execute_wrapper)
