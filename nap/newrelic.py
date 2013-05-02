bcad398672744092bd4a33e06d09b996ba4c4762
Curtis <curtis@tinbrain.net>
2013-05-02 15:37:26 +1000

    Avoid import clashes in newrelic module

--- nap/newrelic.py
+++ nap/newrelic.py
@@ -5,30 +5,31 @@
 # [import-hook:nap.publisher]
 # enabled = true
 # execute = newrelic_django_nap:instrument_django_nap_publisher
- 
+from __future__ import absolute_import
+
 from newrelic.agent import (current_transaction, set_transaction_name,
     FunctionTrace, callable_name, ObjectWrapper)
- 
+
 def instrument_django_nap_publisher(module):
- 
+
     def execute_wrapper(wrapped, instance, args, kwargs):
- 
+
         transaction = current_transaction()
- 
+
         if transaction is None:
             return wrapped(*args, **kwargs)
- 
+
         def _handler(handler, *args, **kwargs):
             return handler
- 
+
         handler = _handler(*args, **kwargs)
- 
+
         name = callable_name(handler)
- 
+
         set_transaction_name(name)
- 
+
         with FunctionTrace(transaction, name):
             return wrapped(*args, *kwargs)
- 
+
     module.BasePublisher.execute = ObjectWrapper(
         module.BasePublisher.execute, None, execute_wrapper)
