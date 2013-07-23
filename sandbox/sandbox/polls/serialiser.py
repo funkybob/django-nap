5ee94e9a74bc4128ed8e7e10a2106ea422f22757
Curtis Maloney <curtis@tinbrain.net>
2013-07-23 12:42:10 +1000

    Add attribute to choices field declaration

--- sandbox/sandbox/polls/serialiser.py
+++ sandbox/sandbox/polls/serialiser.py
@@ -16,7 +16,7 @@ class PollSerialiser(serialiser.Serialiser):
 
     question = fields.Field()
     published = fields.DateTimeField('pub_date')
-    choices = fields.ManySerialiserField(serialiser=ChoiceSerialiser())
+    choices = fields.ManySerialiserField('choices_set.all', serialiser=ChoiceSerialiser())
 
 
 class PollPublisher(publisher.Publisher):
