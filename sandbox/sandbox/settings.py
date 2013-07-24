4b0d9a7aeeb1c22165abc46a3935d9b0ca61d06c
Curtis Maloney <curtis@tinbrain.net>
2013-07-24 13:52:11 +1000

    Enable admin

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -113,12 +113,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'sandbox.polls',
-    'django_extensions'
+    'polls',
 )
 
 # A sample logging configuration. The only tangible logging
