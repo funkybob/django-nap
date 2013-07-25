381b4f72fc2803d4a03337fd3ea5c9c0c8311a1e
Curtis Maloney <curtis@tinbrain.net>
2013-07-25 17:59:38 +1000

    datetime.isoformat requires byte string, not unicode

--- nap/fields.py
+++ nap/fields.py
@@ -63,7 +63,7 @@ class DecimalField(Field):
 class DateTimeField(Field):
 
     def reduce(self, value, **kwargs):
-        return value.replace(microsecond=0).isoformat(' ')
+        return value.replace(microsecond=0).isoformat(b' ')
 
     def restore(self, value, **kwargs):
         return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
