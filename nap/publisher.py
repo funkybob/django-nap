eb107f09f006778cb96983479f2e9102d48193dc
Curtis <curtis@tinbrain.net>
2013-08-18 16:52:01 +1000

    Remove unused import

--- nap/publisher.py
+++ nap/publisher.py
@@ -6,7 +6,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-import collections
 from collections import defaultdict
 
 from . import http
