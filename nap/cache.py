641c5965d42fe65d07f855f0f8faa32c3a822362
Curtis <curtis@tinbrain.net>
2013-08-18 16:57:26 +1000

    Lookup cache by key

--- nap/cache.py
+++ nap/cache.py
@@ -14,7 +14,7 @@ class CachedSerialiser(object):
         '''Add object caching'''
         # This currently assumes obj is a Model instance
         cache_key = 'api_%s_%d' % (self.__class__.__name__, obj.pk)
-        data = self._meta.cache.get(data)
+        data = self._meta.cache.get(cache_key)
         if data is None:
             data = super(CachedSerialiser, self).object_deflate(obj, **kwargs)
             self._meta.cache.set(data, timeout=getattr(self._meta, 'timeout', None))
