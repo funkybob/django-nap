4142bfcc17282a02f3e4450bc7eb372d42737186
Curtis <curtis@tinbrain.net>
2013-09-12 15:35:34 +1000

    Avoid issues with current cache API limitations

--- nap/cache.py
+++ nap/cache.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
 
-from django.core.cache import get_cache
+from django.core.cache import get_cache, cache
 from six import string_types
 
 class CachedSerialiser(object):
     '''Mixin to cache per-object serialised data'''
     def __init__(self, *args, **kwargs):
-        cache = getattr(self._meta, 'cache', 'default')
+        cache = getattr(self._meta, 'cache', cache)
         if isinstance(cache, string_types):
             self._meta.cache = get_cache(cache)
 
