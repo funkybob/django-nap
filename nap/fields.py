5714e7dae467fec08e94223c3c86ec73c5db3f81
Curtis Maloney <curtis@tinbrain.net>
2013-11-22 11:46:38 +1100

    Fix fix for py3 compatibility

--- nap/fields.py
+++ nap/fields.py
@@ -88,7 +88,7 @@ class DecimalField(Field):
 class DateTimeField(Field):
 
     def reduce(self, value, **kwargs):
-        return value.replace(microsecond=0).isoformat(' ')
+        return value.replace(microsecond=0).isoformat(str(' '))
 
     def restore(self, value, **kwargs):
         return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
