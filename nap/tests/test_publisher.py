edb1c810b1fce02e9331f077d17836df1a61ea53
Curtis <curtis@tinbrain.net>
2013-11-24 14:33:16 +1100

    Start publisher tests

--- nap/tests/test_publisher.py
+++ nap/tests/test_publisher.py
@@ -1,4 +1,22 @@
 
 from django.test import TestCase
+from django.test.client import RequestFactory
+
+from nap import publisher
+
+class TestPublisher(publisher.Publisher):
+    pass
 
 
+class PublisherTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.request = self.factory.get('/')
+        self.pub = TestPublisher(self.request)
+
+    def test_000_index(self):
+        data = self.pub.index()
+
+        self.assertTrue('list' in data)
+        self.assertTrue('object' in data)
