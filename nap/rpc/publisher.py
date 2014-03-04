63edaf9a0279add46193c1ba2e4ac365aaba77ab
funkybob <curtis@tinbrain.net>
2014-03-04 11:43:13 +1100

    Start adding RPC publisher

---
+++ nap/rpc/publisher.py
@@ -0,0 +1,32 @@
+
+from nap.publisher import Publisher
+
+class RpcPublisher(Publisher):
+    '''
+    RPC Style Publisher.
+
+    Single URL endpoint, determines handler based on X-RPC-Action header and
+    HTTP method.
+    '''
+
+    @classmethod
+    def patterns(cls, api_name=None):
+        view = cls.build_view()
+
+        if api_name:
+            name = '%s_%s' % (api_name, cls.api_name)
+        else:
+            name = cls.api_name
+
+        return [
+            url(r'^$', view, name='%s_endpoint' % name
+        ]
+
+    def dispatch(self, request):
+        self.action = request.HTTP['X-RPC-ACTION']
+        try:
+            handler = getattr(self, 'do_%s' % action, None)
+        except AttributeError:
+            return http.NotFound()
+
+        return self.execute(handler)
