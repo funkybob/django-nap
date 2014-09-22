b686d316ae81595a26bcc02c19a906ad55f0a00c
Curtis <curtis@tinbrain.net>
2014-09-22 12:55:25 +1000

    Modernise test structure

---
+++ runtests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Adapted from https://raw.githubusercontent.com/hzy/django-polarize/master/runtests.py
+
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        INSTALLED_APPS=(
+            'tests',
+        )
+    )
+
+
+def runtests():
+    argv = sys.argv[:1] + ['test', 'tests']
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    runtests()
