24622b4dc84fa733096e8ad6824f55f4ec5808ae
funkybob <curtis@tinbrain.net>
2014-10-10 17:59:57 +1100

    Begin reworking rest/views to be CBV

--- nap/rest/views.py
+++ nap/rest/views.py
@@ -1,45 +1,17 @@
 from __future__ import unicode_literals
 
-from .publisher import Publisher, SimplePatternsMixin
-from .models import ModelPublisher
-
-from django.template.response import TemplateResponse
+from .. import http
 
 
-class ViewPublisherMixin(object):
+class SerialisedResponseMixin(object):
     '''
-    A Publisher for non-API views.
-
-    Replaces create_response with a wrapper around TemplateResponse.
+    Passes context data through a
     '''
-    api_name = None
+    content_type = 'application/json'
+    response_class = http.JsonResponse
 
-    def create_response(self, content, **response_kwargs):
-        '''Return a response serialising the content'''
-        template = response_kwargs.pop('template', None)
-        if not template:
-            template = self.get_template_names()
-        return TemplateResponse(self.request, template, content,
-            **response_kwargs
-        )
+    def render_to_response(self, context, **response_kwargs):
+        response_class = response_kwargs.pop('response_class', self.response_class)
+        return response_class(context, **response_kwargs)
 
-
-class ViewPublisher(SimplePatternsMixin, Publisher):
-    def get_template_name(self):
-        return self.template_name % {
-            'action': self.action,
-            'method': self.method,
-            'mode': self.mode,
-        }
-
-
-class ModelViewPublisher(SimplePatternsMixin, ModelPublisher):
-    model = None
-
-    def get_template_name(self):
-        '''Return the template name to use for this view.'''
-        return '%s/%s_%s.html' % (
-            self.api_name or self.model._meta.app_label,
-            self.model._meta.model_name,
-            self.action,
-        )
+# TODO Add mixins for CRUD stages to process data through serialisers
