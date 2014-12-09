550106fbff26c16cdf2269dc0778814c05ed1e3b
bobobo1618 <bobobo1618@users.noreply.github.com>
2014-12-10 10:25:40 +1100

    Fix to include mandatory name attribute

--- nap/apps.py
+++ nap/apps.py
@@ -6,6 +6,8 @@ from django.utils.module_loading import autodiscover_modules
 
 class NapConfig(AppConfig):
     '''App Config that performs auto-discover on ready.'''
+    
+    name = 'nap'
 
     def ready(self):
         super(NapConfig, self).ready()
