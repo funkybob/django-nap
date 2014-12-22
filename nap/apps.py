f6e38619fa8129db100b774523b3b0207510a6cc
funkybob <curtis@tinbrain.net>
2014-12-22 12:26:49 +1100

    Remove trailing whitespace

--- nap/apps.py
+++ nap/apps.py
@@ -6,7 +6,7 @@ from django.utils.module_loading import autodiscover_modules
 
 class NapConfig(AppConfig):
     '''App Config that performs auto-discover on ready.'''
-    
+
     name = 'nap'
 
     def ready(self):
