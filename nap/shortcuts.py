84c3992478333269002ce8df6fec175783ed1738
funkybob <curtis@tinbrain.net>
2015-01-23 09:33:25 +1100

    Add shortcuts module that replaces get_[object|list]_or_404 to raise http.NotFound

---
+++ nap/shortcuts.py
@@ -0,0 +1,41 @@
+
+'''
+A wrapper for django shortcuts that adapts responses to be from nap.http
+'''
+
+from django.shortcuts import *
+
+from nap import http
+
+
+def get_object_or_404(klass, *args, **kwargs):
+    """
+    Uses get() to return an object, or raises a Http404 exception if the object
+    does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Note: Like with get(), an MultipleObjectsReturned will be raised if more than one
+    object is found.
+    """
+    queryset = _get_queryset(klass)
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        raise http.NotFound()
+
+
+def get_list_or_404(klass, *args, **kwargs):
+    """
+    Uses filter() to return a list of objects, or raise a Http404 exception if
+    the list is empty.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the filter() query.
+    """
+    queryset = _get_queryset(klass)
+    obj_list = list(queryset.filter(*args, **kwargs))
+    if not obj_list:
+        raise http.NotFound()
+    return obj_list
