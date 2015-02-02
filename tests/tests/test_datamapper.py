2f4b9264722e6ab4f6b366325fd7741559350b1a
funkybob <curtis@tinbrain.net>
2015-02-03 10:28:28 +1100

    Continue datamapper tests

--- tests/tests/test_datamapper.py
+++ tests/tests/test_datamapper.py
@@ -2,17 +2,17 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from nap import datamapper
-from nap.datamapper.filters import NotNullFilter
+from nap.datamapper import DataMapper, field, Field
+from nap.datamapper.filters import NotNullFilter, BooleanFilter, IntegerFilter
 
 
-class TestMapper(datamapper.DataMapper):
-    @datamapper.field
+class TestMapper(DataMapper):
+    @field
     def readonly(self):
         return True
 
-    value = datamapper.Field('value')
-    required = datamapper.Field('required', required=True)
+    value = Field('value')
+    required = Field('required', required=True)
 
 
 class TestObj(object):
@@ -50,3 +50,55 @@ class MapperTest(TestCase):
 
         with self.assertRaises(ValidationError):
             m._apply({}, full=True)
+
+
+class FilterTest(TestCase):
+
+    def test_000_not_null(self):
+        class DM(DataMapper):
+            f = Field('f', filters=[NotNullFilter])
+            g = Field('g', filters=[NotNullFilter])
+
+        m = DM()
+
+        with self.assertRaises(ValidationError):
+            m._apply({'f': None, 'g': 1})
+
+    def test_001_boolean(self):
+        class DM(DataMapper):
+            f = Field('f', filters=[BooleanFilter])
+            g = Field('g', filters=[BooleanFilter])
+            h = Field('h', filters=[BooleanFilter])
+
+        m = DM()
+
+        m._apply({
+            'f': 'False',
+            'g': True,
+            'h': None,
+        })
+
+        self.assertFalse(m._obj.f)
+        self.assertTrue(m._obj.g)
+        self.assertTrue(m._obj.h is None)
+
+    def test_002_integer(self):
+        class DM(DataMapper):
+            f = Field('f', filters=[IntegerFilter])
+            g = Field('g', filters=[IntegerFilter])
+            h = Field('h', filters=[IntegerFilter])
+
+        m = DM()
+
+        m._apply({
+            'f': 1,
+            'g': '1',
+            'h': None,
+        })
+
+        self.assertEqual(m._obj.f, 1)
+        self.assertEqual(m._obj.g, 1)
+        self.assertTrue(m._obj.h is None)
+
+        with self.assertRaises(ValidationError):
+            m._apply({'f': 'test'})
