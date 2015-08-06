bb522a33096d7db252c02fda02e6419548094813
Curtis Maloney <curtis@tinbrain.net>
2015-08-06 22:52:29 +1000

    Use discover runner on older djangos

--- runtests.py
+++ runtests.py
@@ -7,6 +7,15 @@ import sys
 from django.conf import settings
 from django.core.management import execute_from_command_line
 
+import django
+
+if django.VERSION < (1, 6):
+    extra_settings = {
+        'TEST_RUNNER': 'discover_runner.DiscoverRunner',
+    }
+else:
+    extra_settings = {}
+
 
 if not settings.configured:
     settings.configure(
@@ -21,6 +30,7 @@ if not settings.configured:
         MIDDLEWARE_CLASSES=(
         ),
         ROOT_URLCONF='tests.urls',
+        **extra_settings,
     )
 
 
