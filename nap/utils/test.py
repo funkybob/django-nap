7d0245a246cb70722b3d07a48336697c50a2d354
Curtis Maloney <curtis@tinbrain.net>
2015-08-25 21:42:53 +1000

    Move utils to a module, and add test helpers

---
+++ nap/utils/test.py
@@ -0,0 +1,21 @@
+import json
+
+from django.test import TestCase, Client
+
+
+class JsonClient(Client):
+
+    def generic(self, method, path, data='', *args, **kwargs):
+        if 'json' in kwargs:
+            if data:
+                raise ValueError("Must specify at most ond of 'data' and 'json'.")
+            data = json.dumps(kwargs.pop('json'))
+            kwargs.setdefault('content_type', 'text/json')
+        return super(Client, self).generic(method, path, data, *args, **kwargs)
+
+
+class ApiTestCase(TestCase):
+
+    def assertResponseCode(self, url, status_code, method='get', **kwargs):
+        result = getattr(self.client, method)(url, **kwargs)
+        self.assertEqual(result.status_code, status_code)
