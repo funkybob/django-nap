06561ea6f6a823984de832ddec44b1a819905226
Curtis Maloney <curtis@tinbrain.net>
2016-05-17 22:49:44 +1000

    Update urls in tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import include, url
 from . import rpc_views
 from . import rest_views
 
 from nap.rest import api
 #api.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^api/', include(api.patterns(True))),
-    (r'^rpc/', rpc_views.View.as_view()),
-    (r'^rest/polls/(?P<pk>\d+)$', rest_views.SinglePollView.as_view()),
-    (r'^rest/polls/$', rest_views.PollListView.as_view()),
-)
+urlpatterns = [
+    url(r'^api/', include(api.patterns(True))),
+    url(r'^rpc/', rpc_views.View.as_view()),
+    url(r'^rest/polls/(?P<pk>\d+)$', rest_views.SinglePollView.as_view()),
+    url(r'^rest/polls/$', rest_views.PollListView.as_view()),
+]
