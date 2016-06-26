33d93e46e9cbaedd2ea692e23192a5c5849f0b0a
Curtis Maloney <curtis@tinbrain.net>
2016-06-27 09:48:37 +1000

    Add AuthorisationMixin class

--- nap/rest/auth.py
+++ nap/rest/auth.py
@@ -2,6 +2,17 @@
 from nap import auth
 
 
+class AuthorisationMixin(object):
+    '''
+    Mixin class to help decorating the view function returned by ``as_view``.
+    '''
+
+    @classonlymethod
+    def as_view(cls, *args, **kwargs):
+        view = super(LoginView, cls).as_view(*args, **kwargs)
+        return self.permit(view)
+
+
 class LoginRequiredMixin(object):
 
     @auth.permit_logged_in
