eaec243cd4e29957648bcc536ebee3a0a8e32164
Curtis Maloney <curtis@tinbrain.net>
2016-08-01 12:23:55 +1000

    Drop use of old auth module

--- nap/rest/auth.py
+++ nap/rest/auth.py
@@ -1,27 +1,40 @@
+from django.utils.decorators import classonlymethod
 
-from nap import auth
+from nap import http
 
 
 class AuthorisationMixin(object):
     '''
     Mixin class to help decorating the view function returned by ``as_view``.
     '''
+    auth_response_class = http.Forbidden
 
     @classonlymethod
     def as_view(cls, *args, **kwargs):
         view = super(LoginView, cls).as_view(*args, **kwargs)
         return self.permit(view)
 
+    @classonlymethod
+    def permit(cls, view):
+        @wraps(view)
+        def decorator(request, *args, **kwargs):
+            if self.auth_test(request, *args, **kwargs):
+                return view(request, *args, **kwargs)
+            return self.auth_response_class()
+        return decorator
+
+    @classonlymethod
+    def auth_test(cls, request, *args, **kwargs):
+        return True
+
 
 class LoginRequiredMixin(object):
 
-    @auth.permit_logged_in
-    def dispatch(self, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+    def auth_test(cls, request, *args, **kwargs):
+        return request.user.is_authenticated()
 
 
 class StaffRequiredMixin(object):
 
-    @auth.permit_staff
-    def dispatch(self, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+    def auth_test(cls, request, *args, **kwargs):
+        return request.user.is_staff
