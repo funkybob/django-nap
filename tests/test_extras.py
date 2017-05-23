5dd87f366076e95f1c0ffaf708353ec97f3b9a24
Curtis Maloney <curtis@tinbrain.net>
2017-05-23 20:29:33 +1000

    Add tests for extras

---
+++ tests/test_extras.py
@@ -0,0 +1,21 @@
+from types import SimpleNamespace
+
+from django.test import TestCase
+
+from . import models
+
+from nap import mapper
+from nap.extras import actions
+
+
+class ActionTestCase(TestCase):
+    def test_smoke(self):
+        class M(mapper.ModelMapper):
+            class Meta:
+                model = models.Choice
+                fields = '__all__'
+
+        action = actions.ExportCsv(M)
+
+        resp = action(SimpleNamespace(model=models.Choice), None, models.Choice.objects.all())
+        output = resp.getvalue()
