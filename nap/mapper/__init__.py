b1e0f127f5c891ab6ceaf1a050fc05e87f65e8a1
Curtis Maloney <curtis@tinbrain.net>
2017-05-26 17:52:08 +1000

    Bring more things to the parent namespace

--- nap/mapper/__init__.py
+++ nap/mapper/__init__.py
@@ -3,5 +3,5 @@ from .fields import (  # NOQA
     BooleanField, DateField, DateTimeField, Field, FloatField, IntegerField,
     MapperField, TimeField, context_field, field,
 )
-from .models import ModelMapper  # NOQA
+from .models import ModelMapper, ToOneField, ToManyField  # NOQA
 from .utils import DictObject  # NOQA
