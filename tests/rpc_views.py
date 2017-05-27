a7b8376882ff330fb3b7076e24162290606c7061
Curtis Maloney <curtis@tinbrain.net>
2017-05-27 21:03:46 +1000

    Add RPC options test

--- tests/rpc_views.py
+++ tests/rpc_views.py
@@ -2,6 +2,7 @@ from nap import rpc
 
 
 class View(rpc.RPCView):
+    permit_introspect = True
 
     @rpc.method
     def echo(self, **kwargs):
--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, LiveServerTestCase
 
+from nap.http import STATUS
 from nap.rpc import client
 
 import json
@@ -16,21 +17,27 @@ class RPCTest(TestCase):
             kwargs['data'] = data
         return self.client.post('/rpc/', **kwargs)
 
+    def test_options(self):
+        r = self.client.options('/rpc/')
+        self.assertEqual(r.status_code, STATUS.OK)
+        data = json.loads(r.content.decode())
+        self.assertTrue('echo' in data)
+
     def test_body_omitted(self):
         r = self.call(None)
         self.assertEqual(r.content.decode('utf-8'), '{}')
 
     def test_body_not_json(self):
         r = self.call('this is not actually json')
-        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.status_code, STATUS.BAD_REQUEST)
 
     def test_body_not_map(self):
         r = self.call('["this is a list rather than","a dictionary"]')
-        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.status_code, STATUS.BAD_REQUEST)
 
     def test_body_not_string_keys(self):
         r = self.call('{"key":"value",42:"uh oh"}')
-        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.status_code, STATUS.BAD_REQUEST)
 
     def test_basic(self):
         r = self.call(json.dumps({'foo': 'bar', 'baz': 42}))
@@ -46,11 +53,11 @@ class RPCTest(TestCase):
 
     def test_no_action(self):
         r = self.call(None, action=None)
-        self.assertEqual(r.status_code, 405)
+        self.assertEqual(r.status_code, STATUS.METHOD_NOT_ALLOWED)
 
     def test_bad_action(self):
         r = self.call(None, action='bad')
-        self.assertEqual(r.status_code, 412)
+        self.assertEqual(r.status_code, STATUS.PRECONDITION_FAILED)
 
 
 class RPCClientTest(LiveServerTestCase):
