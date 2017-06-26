e637fc44458f2e97ee893462ffb89d7ada93df05
Curtis Maloney <curtis@tinbrain.net>
2017-06-26 16:24:06 +1000

    Bumped version to 0.30.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as fin:
 
 setup(
     name='django-nap',
-    version='0.30.8',
+    version='0.30.9',
     description='A light REST tool for Django',
     long_description=readme,
     author='Curtis Maloney',
