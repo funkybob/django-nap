ad0a3234a10ff2d57baad5d5428a04cd63e70a68
Curtis Maloney <curtis@tinbrain.net>
2017-12-03 11:21:37 +1100

    Update models in tests

--- tests/models.py
+++ tests/models.py
@@ -8,6 +8,6 @@ class Poll(models.Model):
 
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
